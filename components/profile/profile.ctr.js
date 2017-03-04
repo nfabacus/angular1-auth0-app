d808d9fe2f994058a78c30e956380f72b94b728f
noby <n.fujioka@abacusmaths.info>
2017-03-04 21:36:49 +0000

    added simple angular app

---
+++ components/profile/profile.ctr.js
@@ -0,0 +1,14 @@
+(function(){
+
+  'use strict';
+
+  angular
+  .module('authApp')
+  .controller('profileController', profileController);
+
+  function profileController($http){
+
+    var vm = this;  
+    vm.message = "hello!";
+  }
+})();
